8223782c4a6ec432fd3c07d1b4ea2d3d99df7b40
SrFranciscus <fotologyfacebook@gmail.com>
2024-07-30 13:29:56 -0400

    Encriptador de texto

---
+++ script.js
@@ -0,0 +1,8 @@
+const sectionDos = document.getElementById("seccionDos")
+const textarea = document.getElementById("areaMensajeUsuario")
+
+function mostrarSeccion() {
+  seccionDos.style.display = "block"
+  seccionDos.style.border = "green 1px solid"
+  seccionDos.style.borderRadius = "30px"
+}
